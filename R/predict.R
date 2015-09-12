a310f28c890ff14bdeaa48ba3285b22ff116decd
Shui Hu <shui1234@gmail.com>
2015-09-11 17:33:00 -0700

    Working

--- R/predict.R
+++
@@ -1,2 +0,0 @@
-# This should be part of Splus proper -- make predict a method
-predict <- function(tree, ...)  UseMethod("predict")
--- R/test2.R
+++ R/test2.R
@@ -1,4 +1,5 @@
 test2 <- function() {
+  library(rpart)
   age <- sample(1:50, 20, replace = T)
   education <- sample(1:12, 20, replace = T)
   black <- sample(0:1, 20, replace = T)
@@ -7,9 +8,6 @@ test2 <- function() {
   mini <- data.frame(age, education, black, married, income)
   
   mtree <- rpart(income~., data = mini, method = "anova", cp = 0, minbucket = 1, xval = 0)
-  mtree2 <- mtree
-  prune.rpart(mtree2, cp = 0.1)
-  class(mtree)
   predict(mtree, mini)
 }
 #predict.rpart(mtree, mini)
