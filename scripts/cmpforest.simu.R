dea0d24b04fbd7de835e21a8360377cf772cbd63
Stefan Wager <swager@stanford.edu>
2015-09-12 12:06:07 +0200

    positive experiment

--- scripts/cmpforest.simu.R
+++ scripts/cmpforest.simu.R
@@ -1,19 +1,23 @@
-#rm(list = ls())
+rm(list = ls())
 
-n = 1000
-sigma = 1
-d = 2
+library(ggplot2)
+
+n = 20000
+sigma = 0.1
+d = 10
 k = 2
 
 # heterogeneous effect of treatment
 effect = function(x) {
-	4 * prod(x[1:k])
+	4 * prod(sign(x[1:k]) * sqrt(abs(x[1:k]))) 
 }
 
 # baseline effect of features
-baseline = function(x) {
-	2 * sum(x[1:k]^2) + 10 * sum(abs(x[1:k]))
-}
+#baseline = function(x) {
+#	2 * sum(x[1:k]^2) + 10 * sum(abs(x[1:k]))
+#}
+
+baseline = function(x) { 0 }
 
 X = matrix(runif(n * d, -1, 1), n, d) # features
 W = rbinom(n, 1, 0.5) #treatment condition
@@ -24,6 +28,23 @@ n.test = 10000
 X.test = matrix(runif(n.test * d, -1, 1), n.test, d)
 true.eff = apply(X.test, 1, effect)
 
-cmp = comparisonForest(Y, X, W, X.test = X.test, num.trees = 1000, sample.size = n / 10)
+cmp = comparisonForest(Y, X, W, X.test = X.test, num.trees = 100, sample.size = n / 10)
 
 plot(true.eff, cmp$new.tau)
+abline(0, 1, col = 2, lwd = 2)
+
+minp = min(true.eff, cmp$new.tau)
+maxp = max(true.eff, cmp$new.tau)
+rngp = maxp - minp
+
+ncol = 100
+
+true.scl = pmax(ceiling(ncol * (true.eff - minp) / rngp), 1)
+fit.scl = pmax(ceiling(ncol * (cmp$new.tau - minp) / rngp), 1)
+
+hc = heat.colors(ncol)
+plot(x=X.test[,1], y=X.test[,2], pch = 16, col = hc[true.scl])
+plot(x=X.test[,1], y=X.test[,2], pch = 16, col = hc[fit.scl])
+
+
+
