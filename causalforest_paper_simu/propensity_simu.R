26911e0c32da9494c2c57e126cf7b3d2000c34b7
Stefan <swager@stanford.edu>
2015-10-08 19:55:08 -0700

    +knn for propensity

--- causalforest_paper_simu/propensity_simu.R
+++ causalforest_paper_simu/propensity_simu.R
@@ -8,18 +8,13 @@ library(Hmisc)
 rm(list = ls())
 
 n = 500
-ntree = 1000
+ntree = 20 #1000
 sigma = 1
 
 n.test = 100
 
 dvals = c(2, 5, 10, 15, 20)
 simu.reps = 10
-
-# heterogeneous effect of treatment
-effect = function(x) {
-	0
-}
 
 baseline = function(x) {
 	2 * (x[1] - 0.5)
@@ -34,35 +29,83 @@ simu.fun = function(d) {
 X = matrix(runif(n * d, 0, 1), n, d) # features
 e = apply(X, 1, propensity)
 W = rbinom(n, 1, e) #treatment condition
-Y = apply(X, 1, baseline) +  (W - 0.5) * apply(X, 1, effect) + sigma * rnorm(n)
 
+# no treatment effect
+Y = apply(X, 1, baseline) + sigma * rnorm(n)
 
 X.test = matrix(runif(n.test * d, 0, 1), n.test, d)
-true.eff = apply(X.test, 1, effect)
+
+
+#
+# random forest
+#
 
 forest = propensityForest(X, Y, W, num.trees = ntree, sample.size = n / 10, nodesize = 1)
 predictions = predict(forest, X.test)
-
 forest.ci = randomForestInfJack(forest, X.test, calibrate = TRUE)
 
+# true effect is 0
 se.hat = sqrt(forest.ci$var.hat)
-up.lim = predictions + 1.96 * se.hat
-down.lim = predictions - 1.96 * se.hat
+rf.covered = mean(abs(predictions) <= 1.96 * se.hat)
+rf.mse = mean(predictions^2)
 
-covered = (true.eff <= up.lim) & (true.eff >= down.lim)
+k.small = 10
+knn.0.mu = knn.reg(X[W==0,], X.test, Y[W==0], k = k.small)$pred
+knn.1.mu = knn.reg(X[W==1,], X.test, Y[W==1], k = k.small)$pred
 
-mean(covered)
+knn.0.mu2 = knn.reg(X[W==0,], X.test, Y[W==0]^2, k = k.small)$pred
+knn.1.mu2 = knn.reg(X[W==1,], X.test, Y[W==1]^2, k = k.small)$pred
 
+knn.0.var = (knn.0.mu2 - knn.0.mu^2) / (k.small - 1)
+knn.1.var = (knn.1.mu2 - knn.1.mu^2) / (k.small - 1)
+
+knn.tau = knn.1.mu - knn.0.mu
+knn.se = sqrt(knn.0.var + knn.1.var)
+knn.covered.v1 = mean(abs(knn.tau) <= 1.96 * knn.se)
+knn.covered.v2 = mean(abs(knn.tau) <= 1.96 * sqrt(mean(knn.0.var + knn.1.var)))
+knn.mse = mean(knn.tau^2)
+
+k.big = 100
+knnbig.0.mu = knn.reg(X[W==0,], X.test, Y[W==0], k = k.big)$pred
+knnbig.1.mu = knn.reg(X[W==1,], X.test, Y[W==1], k = k.big)$pred
+
+knnbig.0.mu2 = knn.reg(X[W==0,], X.test, Y[W==0]^2, k = k.big)$pred
+knnbig.1.mu2 = knn.reg(X[W==1,], X.test, Y[W==1]^2, k = k.big)$pred
+
+knnbig.0.var = (knnbig.0.mu2 - knnbig.0.mu^2) / (k.big - 1)
+knnbig.1.var = (knnbig.1.mu2 - knnbig.1.mu^2) / (k.big - 1)
+
+knnbig.tau = knnbig.1.mu - knnbig.0.mu
+knnbig.se = sqrt(knnbig.0.var + knnbig.1.var)
+knnbig.covered.v1 = mean(abs(knnbig.tau) <= 1.96 * knnbig.se)
+knnbig.covered.v2 = mean(abs(knnbig.tau) <= 1.96 * sqrt(mean(knnbig.0.var + knnbig.1.var)))
+knnbig.mse = mean(knnbig.tau^2)
+
+
+c(rf.covered = rf.covered,
+           rf.mse = rf.mse,
+           knn.covered.v1 = knn.covered.v1,
+           knn.covered.v2 = knn.covered.v2,
+           knn.mse = knn.mse,
+           knnbig.covered.v1 = knnbig.covered.v1,
+           knnbig.covered.v2 = knnbig.covered.v2,
+           knnbig.mse = knnbig.mse)
 }
 
-results = sapply(dvals, function(d) {
+results.raw = lapply(dvals, function(d) {
 	print(paste("NOW RUNNING:", d))
-	res.d = replicate(simu.reps, simu.fun(d))
-	print(paste("RESULT AT", d, "IS", mean(res.d)))
-	res.d
+	res.d = sapply(1:simu.reps, function(iter) simu.fun(d))
+	res.fixed = data.frame(t(res.d))
+	print(paste("RESULT AT", d, "IS", colMeans(res.fixed)))
+	res.fixed
 })
 
-colnames(results) = dvals
-colMeans(results)
+results.condensed = lapply(results.raw, function(RR) {
+	RR.mu = colMeans(RR)
+	RR.var = sapply(RR, var) / (nrow(RR) - 1)
+	rbind("mu"=RR.mu, "se"=sqrt(RR.var))
+})
+
+results.condensed
 
 save.image("~/causal_effect_estimation/causalforest_paper_simu/propensity.RData")
