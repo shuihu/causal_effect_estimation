a3f36fe5371c6de262716cba3a2aa1a9e824bcb4
susanathey <athey@stanford.edu>
2015-10-31 13:28:22 -0700

    Revert "Revert "fix bug in anova2""
    This reverts commit 9c0eb648fb79ca0735c448e9ba76a95f9bb59143.

--- src/anova2.c
+++ src/anova2.c
@@ -48,6 +48,8 @@ anovass2(int n, double *y[], double *value, double *risk, double *wt, double *tr
     double temp = 0., temp0 = 0., temp1 = 0., twt = 0.; /* sum of the weights */ 
     double ttreat = 0.;
     double effect;
+    double tr_var, con_var;
+    double con_sqr_sum = 0., tr_sqr_sum = 0.;
     // double ss;
 
     for (i = 0; i < n; i++) {
@@ -56,11 +58,15 @@ anovass2(int n, double *y[], double *value, double *risk, double *wt, double *tr
       temp0 += *y[i] * wt[i] * (1 - treatment[i]);
       twt += wt[i];
       ttreat += wt[i] * treatment[i];
+      tr_sqr_sum += (*y[i]) * (*y[i]) * wt[i] * treatment[i];
+      con_sqr_sum += (*y[i]) * (*y[i]) * wt[i] * (1- treatment[i]);
     }
     //mean = temp / twt;
     //effect = temp1 / twt - temp0 / (n - twt);
     effect = temp1 / ttreat - temp0 / (twt - ttreat);
-
+    tr_var = tr_sqr_sum / ttreat - temp1 * temp1 / (ttreat * ttreat);
+    con_var = con_sqr_sum / (twt - ttreat) - temp0 * temp0 / ((twt - ttreat) * (twt - ttreat));
+    
     /* 
     ss = 0;
     for (i = 0; i < n; i++) {
@@ -68,12 +74,11 @@ anovass2(int n, double *y[], double *value, double *risk, double *wt, double *tr
 	    ss += temp * temp * wt[i];
     } 
     */
-
     *value = effect;
     //*risk = 4 * n * MAX * MAX - effect * effect * n;
     //*risk = n * MAX * MAX - effect * effect * n;
     //max_y = MAX;
-    *risk = 4 * n * max_y * max_y - n * effect * effect ;
+    *risk = 4 * n * max_y * max_y - n * effect * effect +  2 * n * (tr_var /ttreat  + con_var / (twt - ttreat));
 }
 
 /*
@@ -135,7 +140,7 @@ anova2(int n, double *y[], double *x, int nclass,
     con_var = (right_sqr_sum - right_tr_sqr_sum) / (right_wt - right_tr)
               - (right_sum - right_tr_sum) * (right_sum - right_tr_sum) / ((right_wt - right_tr) * (right_wt - right_tr));
     //node_effect = temp * temp * n
-    node_effect = temp * temp * n - 2 * (tr_var + con_var) * n;
+    node_effect = temp * temp * n - 2 * n * (tr_var / right_tr  + con_var / (right_wt - right_tr));
     
     //Rprintf("n = %d, node_effect = %f\n", n, node_effect);
     
@@ -197,7 +202,7 @@ anova2(int n, double *y[], double *x, int nclass,
                              - (left_sum - left_tr_sum) * (left_sum - left_tr_sum)/ ((left_wt - left_tr) * (left_wt - left_tr));        
               
               left_effect = left_temp * left_temp * left_n
-              - 2 * (left_tr_var + left_con_var) * left_n;
+              - 2 * left_n * (left_tr_var / left_tr + left_con_var / (left_wt - left_tr));
               
               //taumean = right_tr / right_wt;
               //temp = (right_tr_sum - right_sum * taumean) /
@@ -207,7 +212,7 @@ anova2(int n, double *y[], double *x, int nclass,
               right_con_var = (right_sqr_sum - right_tr_sqr_sum) / (right_wt - right_tr)
                              - (right_sum - right_tr_sum) * (right_sum - right_tr_sum) / ((right_wt - right_tr) * (right_wt - right_tr));
               right_effect = right_temp * right_temp * right_n
-              - 2 * (right_tr_var + right_con_var) * right_n;    
+              - 2 * right_n * (right_tr_var / right_tr + right_con_var / (right_wt - right_tr));    
               
               temp = left_effect + right_effect - node_effect;
               //Rprintf("at %f,leftn: %d, lefteffect: %f, rightn: %d, righteffect: %f\n", x[i], left_n, left_effect,right_n, right_effect, node_effect);
@@ -242,6 +247,7 @@ anova2(int n, double *y[], double *x, int nclass,
     /*
      * Categorical predictor
      */
+     
     else {
       for (i = 0; i < nclass; i++) {
 	      countn[i] = 0;
@@ -332,7 +338,7 @@ anova2(int n, double *y[], double *x, int nclass,
             left_con_var = (left_sqr_sum - left_tr_sqr_sum) / (left_wt - left_tr) 
                              - (left_sum - left_tr_sum) * (left_sum - left_tr_sum)/ ((left_wt - left_tr) * (left_wt - left_tr));        
             left_effect = left_temp * left_temp * left_n
-              - 2 * (left_tr_var + left_con_var) * left_n;
+              - 2 * left_n * (left_tr_var / left_tr + left_con_var / (left_wt - left_tr));
             
              //Rprintf("left_sum = %f, left_wt_sum = %f, left_wt = %f, left_n = %d\n", left_sum, left_wt_sum, left_wt, left_n);             
              right_temp = right_tr_sum / right_tr - (right_sum - right_tr_sum) / (right_wt - right_tr);
@@ -340,7 +346,7 @@ anova2(int n, double *y[], double *x, int nclass,
              right_con_var = (right_sqr_sum - right_tr_sqr_sum) / (right_wt - right_tr)
                              - (right_sum - right_tr_sum) * (right_sum - right_tr_sum) / ((right_wt - right_tr) * (right_wt - right_tr));
              right_effect = right_temp * right_temp * right_n
-              - 2 * (right_tr_var + right_con_var) * right_n; 
+              - 2 * right_n * (right_tr_var / right_tr + right_con_var / (right_wt - right_tr)); 
           
              temp = left_effect + right_effect - node_effect;
             //Rprintf("left_n= %d, lefteffect = %f, right_n = %d, righteffect = %f\n", left_n, left_effect, right_n, right_effect);    
